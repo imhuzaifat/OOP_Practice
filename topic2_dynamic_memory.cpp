3b9a13b5c4adb7e2827b9df82065bd73fa5041d1
Huzaifa Tahir <imhuzaifat@gmail.com>
2026-08-03 22:48:50 +0500

    Added Functions Definitions and Completed Main Function

--- topic2_dynamic_memory.cpp
+++ topic2_dynamic_memory.cpp
@@ -17,3 +17,55 @@
 #include <iostream>
 using namespace std;
 
+int** createMatrix(int rows, int cols);
+void freeMatrix(int** matrix, int rows);
+int** transposeMatrix(int** matrix, int rows, int cols);
+void printMatrix(int** matrix, int rows, int cols);
+
+int main()
+{
+    int rows, cols;
+    cout << "Please Enter the Number of Rows: ";
+    cin >> rows;
+    cout << "Please Enter the Number of Columns: ";
+    cin >> cols;
+
+    // Create and input the original matrix
+    int** matrix = createMatrix(rows, cols);
+
+    // Print original matrix
+    cout << "Original Matrix:" << endl;
+    printMatrix(matrix, rows, cols);
+
+    // Transpose the matrix
+    int** transposedMatrix = transposeMatrix(matrix, rows, cols);
+
+    // Display the transposed matrix
+    cout << "Transposed Matrix:" << endl;
+    printMatrix(transposedMatrix, cols, rows);
+
+    // Free allocated memory
+    freeMatrix(matrix, rows);
+
+    return 0;
+}
+
+int** createMatrix(int rows, int cols)
+{
+
+}
+
+void freeMatrix(int** matrix, int rows)
+{
+
+}
+
+int** transposeMatrix(int** matrix, int rows, int cols)
+{
+
+}
+
+void printMatrix(int** matrix, int rows, int cols)
+{
+    
+}
