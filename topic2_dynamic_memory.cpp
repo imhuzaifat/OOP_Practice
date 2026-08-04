cba9b3a5f157fcbb3f79ad389e3592c34ff954b1
Huzaifa Tahir <imhuzaifat@gmail.com>
2026-08-05 00:11:24 +0500

    Completed Topic 2 File

--- topic2_dynamic_memory.cpp
+++ topic2_dynamic_memory.cpp
@@ -29,7 +29,7 @@ int main()
     cin >> rows;
     cout << "Please Enter the Number of Columns: ";
     cin >> cols;
-
+ 
     // Create and input the original matrix
     int** matrix = createMatrix(rows, cols);
 
@@ -46,26 +46,56 @@ int main()
 
     // Free allocated memory
     freeMatrix(matrix, rows);
+    freeMatrix(transposedMatrix, cols);
 
     return 0;
 }
 
 int** createMatrix(int rows, int cols)
 {
-
-}
-
-void freeMatrix(int** matrix, int rows)
-{
-
+    int** matrix = new int*[rows];
+    for (int i=0; i<rows; i++)
+    {
+        matrix[i] = new int[cols];
+        for (int j=0; j<cols; j++)
+        {
+            cout << "Please Enter the Element (" << i+1 << "," << j+1 << "): ";
+            cin >> matrix[i][j];
+        }
+    }
+    return matrix;
 }
 
 int** transposeMatrix(int** matrix, int rows, int cols)
 {
+    int** transposedMatrix = new int*[cols];
+    for (int i=0; i<cols; i++)
+    {
+        transposedMatrix[i] = new int[rows];
+        for (int j=0; j<rows; j++)
+        {
+            transposedMatrix[i][j] = matrix[j][i];
+        }
+    }
+    return transposedMatrix;
+}
 
+void freeMatrix(int** matrix, int rows)
+{
+    for (int i=0; i<rows; i++)
+        delete[] matrix[i];
+    
+    delete[] matrix;
 }
 
 void printMatrix(int** matrix, int rows, int cols)
 {
-    
+    for (int i=0; i<rows; i++)
+    {
+        for (int j=0; j<cols; j++)
+        {
+            cout << matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
 }
